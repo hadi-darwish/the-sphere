e7969d3744f861976a1f826751fc499b9b0a4304
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-21 21:50:58 +0300

    🟠 => creating a light

--- the-sphere/src/App.jsx
+++ the-sphere/src/App.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import "./App.css";
 
 function App() {
-  const scene = new THREE.Scene();
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const scene = new THREE.Scene();
 
-  //create a sphere
-  const geometry = new THREE.SphereGeometry(5, 32, 32);
-  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-  const sphere = new THREE.Mesh(geometry, material);
-  scene.add(sphere);
+    //create a sphere
+    const geometry = new THREE.SphereGeometry(5, 32, 32);
+    const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+    const sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
 
-  //create a camera
-  const camera = new THREE.PerspectiveCamera(45, 800, 600);
-  scene.add(camera);
+    //create a light
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(0, 5, 10);
+    scene.add(light);
 
-  //create a renderer
-  const canvas = document.querySelector(".webgl");
-  const renderer = new THREE.WebGLRenderer({ canvas });
-  renderer.setSize(800, 600);
-  renderer.render(scene, camera);
+    //create a camera
+    const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+    camera.position.z = 20;
+    scene.add(camera);
+
+    //create a renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(800, 600);
+    renderer.render(scene, camera);
+  });
   return (
     <>
-      <canvas className="webgl"></canvas>
+      <canvas className="webgl" id="webgl" ref={canvasRef}></canvas>
     </>
   );
 }
